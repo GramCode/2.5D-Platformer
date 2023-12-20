f7e978876d9d9e570876c9b0c81f45cc49809aa0
Miguel Pacheco <miguel95_angel@hotmail.com>
2023-12-20 18:40:40 -0400

    Applied Gravity

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,11 +6,17 @@ public class Player : MonoBehaviour
 {
     [SerializeField]
     private float _speed = 5f;
+    [SerializeField]
+    private float _gravity = 1f;
+
     private CharacterController _controller;
 
     void Start()
     {
         _controller = GetComponent<CharacterController>();
+
+        if (_controller == null)
+            Debug.LogError("Character Controller in Player is NULL");
     }
 
     void Update()
@@ -18,8 +24,16 @@ public class Player : MonoBehaviour
         float horizontal = Input.GetAxis("Horizontal");
         Vector3 direction = new Vector3(horizontal, 0, 0);
         Vector3 velocity = direction * _speed;
-        _controller.Move(velocity * Time.deltaTime);
-        
 
+        if (_controller.isGrounded)
+        {
+            //Jump
+        }
+        else
+        {
+            velocity.y -= _gravity;
+        }
+
+        _controller.Move(velocity * Time.deltaTime);
     }
 }
