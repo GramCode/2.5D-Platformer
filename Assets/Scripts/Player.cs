323729435d21307e28f081e412af525baf7de2bd
Miguel Pacheco <miguel95_angel@hotmail.com>
2023-12-21 10:56:32 -0400

    Double Jump

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -7,7 +7,12 @@ public class Player : MonoBehaviour
     [SerializeField]
     private float _speed = 5f;
     [SerializeField]
-    private float _gravity = 1f;
+    private float _gravity = 0.4f;
+    [SerializeField]
+    private float _jumpHeight = 14f;
+
+    private float _yVelocity;
+    private bool _doubleJump = false;
 
     private CharacterController _controller;
 
@@ -27,12 +32,30 @@ public class Player : MonoBehaviour
 
         if (_controller.isGrounded)
         {
+            //_doubleJump = false;
             //Jump
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                _yVelocity = _jumpHeight;
+                _doubleJump = true;
+            }
         }
         else
         {
-            velocity.y -= _gravity;
+            if (_doubleJump == true)
+            {
+                if (Input.GetKeyDown(KeyCode.Space))
+                {
+                    _yVelocity += _jumpHeight;
+                    _doubleJump = false;
+                }
+                
+            }
+            //Apply Gravity
+            _yVelocity -= _gravity;
         }
+
+        velocity.y = _yVelocity;
 
         _controller.Move(velocity * Time.deltaTime);
     }
